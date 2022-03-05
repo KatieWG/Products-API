6498e11dd094c676c4e4321fbea96d71b7e661aa
Katie Gross <kathleen.w.gross@gmail.com>
2022-03-05 18:25:49 -0500

    started server, added sampleData files

--- database/index.js
+++ database/index.js
@@ -6,14 +6,14 @@ let productSchema = mongoose.Schema({
   description: String,
   category: String,
   default_price: Number,
-  features: Array //?
+  features: Array //Features collection
 });
 
 let Product = mongoose.model('Product', productSchema); //THIS IS A MODEL, TEMPLATING WHAT OUR SCHEMAS WILL LOOK LIKE IN OUR COLLECTION
 
 module.exports = {};
-  //comments
 
+//NOTES ->
   // WHEN WE ADD OUR FIRST MODEL INTO OUR COLLECTION, show collections ON THE COMMAND LINE WILL SHOW US OUR COLLECTIONS. OUR COLLECTION WILL BE TITLED THE PLURAL VERSION OF THE WORD WE USE FOR OUR MODEL, SO OUR COLLECTION SHOULD BE TITLED 'Repos', PLURAL FOR 'Repo', OUR MODEL ON LINE 13.
   // db.products.find({}) shows whats inside the collection
   // mongosh opens up mongo interface (similar to mysql)
--- server/index.js
+++ server/index.js
@@ -1,10 +1,37 @@
 const express = require('express');
-const app = extress();
+const app = express();
 const axios = require('axios');
 const path = require('path');
-//jquery?
-//port
+const port = process.env.PORT || 3000;
 
-//app.losten(port, () => {
-//   console.log(`The app server is running on port: ${port}`);
-// })
+const csv = require("csv-parser");
+const createCsvStringifier = require("csv-writer").createObjectCsvStringifier;
+const fs = require('fs');
+const Transform = require("stream").Transform;
+
+
+const csvStringifier = createCsvStringifier({
+  header: [
+    { id: "id", title: "id" },
+    { id: "styleId", title: "styleId" },
+    { id: "url", title: "url" },
+    { id: "thumbnail_url", title: "thumbnail_url" },
+  ],
+});
+
+let readStream = fs.createReadStream("./database/photosSample.csv");
+let writeStream = fs.createWriteStream("./database/photosDestination.csv");
+class CSVCleaner extends Transform {
+  constructor(options) {
+    super(options);
+  }
+}
+
+const transform = function(chunk, encoding, next) {
+  //transform here! :)
+}
+
+app.listen(port, () => {
+  console.log(`The app server is running on port: ${port}!`);
+})
+
